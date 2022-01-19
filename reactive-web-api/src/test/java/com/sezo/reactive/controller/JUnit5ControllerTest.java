db00c08beb722aedbbe2aec232e2304fbe706429
Sezayir Dağtekin <sezayir@hotmail.com>
2022-01-19 11:21:48 +0300

    fix unti test

--- reactive-web-api/src/test/java/com/sezo/reactive/controller/JUnit5ControllerTest.java
+++ reactive-web-api/src/test/java/com/sezo/reactive/controller/JUnit5ControllerTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.TestPropertySource;
@@ -14,6 +15,7 @@ import com.sezo.reactive.model.Product;
 import com.sezo.reactive.repository.ProductRepository;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration
 @TestPropertySource(properties = "spring.mongodb.embedded.version=3.5.5")
 public class JUnit5ControllerTest {
 	
